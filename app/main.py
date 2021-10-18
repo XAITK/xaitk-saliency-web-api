ce8fcf26ca9added3605f4844099cc44ae6887cd
Zack Zeplin <zack.zeplin@gmail.com>
2021-10-17 22:08:07 -0400

    full skeleton, addded basic image calls

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
+from xaitk_saliency.impls.perturb_image.sliding_window import SlidingWindow
+import numpy as np
+import PIL.Image
+import urllib.request
 
 app = FastAPI()
 
@@ -10,9 +13,60 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+#gets an image by url and returns basic data (dimensions and format)
+@app.get("/imageurl")
+def read_image_data(image_url: str):
+    try:
+       image = PIL.Image.open(urllib.request.urlopen(image_url))
+
+       return {"Width": image.size[0], 
+            "Height": image.size[1], 
+            "Format": image.format}
+    except:
+        #there was an error getting the image
+        return {"Error": "incorrecturl"}
+
+#perturbs the image using the xaitk sliding window impl
+@app.get("/perturb/sliding_window")
+def perturb_image(image_url: str):
+    #takes in an image from url and perturbs it, storing resulting masks 
+    #in a ndarray of booleans (this will in turn be used in another call)
+    try:
+        #Getting the image
+        image = PIL.Image.open(urllib.request.urlopen(image_url))
+        #turning into numpy array, 3D array of ints
+        ref_image = np.array(image)
+        #creating a sliding window perturbation class
+        slidingwindow = SlidingWindow()
+        #perturbing the image, saving as 'perturbeddataarray'
+        perturbeddataarray = slidingwindow.perturb(ref_image)
+       
+        return {"Successfully perturbed": 200}
+        
+    except:
+        #error getting the image
+        return {"ERROR": "url"}
+
+@app.post('/perturb')
+async def create_upload_file(file: UploadFile = File(...)):
+    # open the image byte by byte
+    img = PIL.Image.open(file.file)
+
+    # throw image data into an array
+    img_arr = np.array(img)
+
+    # create the sliding window algorithm 
+    slid_algo = SlidingWindow(window_size=(40, 40), stride=(15, 15))
+
+    # create multiple masks of the original image 
+    pert_masks = slid_algo.perturb(img_arr)
+
+    return { 
+            
+            "filename": file.filename,
+            "Pert Masks": pert_masks.shape
+
+    }
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
